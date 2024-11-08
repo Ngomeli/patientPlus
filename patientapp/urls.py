633c69cc76e891cb98601c8caca875e29d099820
Ngomeli <joshuangomeli@gmail.com>
2024-11-08 12:05:43 +0300

    added more files

--- patientapp/urls.py
+++ patientapp/urls.py
@@ -20,5 +20,7 @@ from patientapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
